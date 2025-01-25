08decff8b54c0f62de5d17c717039b91aec7cbf6
Aswajith S <aswajiths777@gmail.com>
2025-01-25 07:08:18 +0530

    update index body -> data

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ CHECKS EXISTANCE OF A USER
 */
 
 app.post("/createuser", (req, res) => {
-  if (!req.data ) return;
+  if (!req.body ) return;
 
   pool.getConnection((err, connection) => {
     if (err) {
@@ -101,7 +101,7 @@ app.post("/createuser", (req, res) => {
     }
 
     connection.query(
-      `CALL CREATE_USER('${req.data["USER_ID"]}','${req.data["PASSWORD"]}')`,
+      `CALL CREATE_USER('${req.body["USER_ID"]}','${req.body["PASSWORD"]}')`,
       (err, rows) => {
         connection.release();
         if (err) {
@@ -117,7 +117,7 @@ app.post("/createuser", (req, res) => {
 
 
 app.post("/changelog", (req, res) => {
-  if (!req.data) return;
+  if (!req.body) return;
 
   pool.getConnection((err, connection) => {
     if (err) {
@@ -126,7 +126,7 @@ app.post("/changelog", (req, res) => {
     }
 
     connection.query(
-      `CALL CREATE_USER('${req.data["USER_ID"]}','${req.data["PASSWORD"]}')`,
+      `CALL CREATE_USER('${req.body["USER_ID"]}','${req.body["PASSWORD"]}')`,
       (err, rows) => {
         connection.release();
         if (err) {
